0eba6e6b9a7997268a07dcf5e913b19722700a41
Roman <roman.pohribniak@gmail.com>
2022-11-27 23:21:45 +0200

    room methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ChatGeteway } from './chat.geteway';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService,ChatGeteway],
+  controllers: [],
+  providers: [ChatGeteway],
 })
 export class AppModule {}
--- src/chat.geteway.ts
+++ src/chat.geteway.ts
@@ -1,12 +1,26 @@
-import { MessageBody,SubscribeMessage, WebSocketGateway,WebSocketServer } from "@nestjs/websockets";
+import { MessageBody,SubscribeMessage, WebSocketGateway,WebSocketServer,OnGatewayConnection} from "@nestjs/websockets";
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatGeteway{
+export class ChatGeteway implements OnGatewayConnection { 
     @WebSocketServer()
     server;
 
-    @SubscribeMessage("message")
-    hadndleMessage(@MessageBody() message:string): void{
-        this.server.emit("message",message)
+    async handleConnection(socket: Socket) {
+      console.log(socket.id)
+    }
+
+    @SubscribeMessage("ENTER_ROOM")
+    enterRoom(socket: Socket,message:any){
+      console.log(message.roomid);
+      socket.join(message.roomid);
+      this.getRooms(socket);
+    }
+    
+    @SubscribeMessage("GET_ROOMS")
+    getRooms(socket: Socket): any{
+      console.log(socket.rooms)
+        this.server.emit("ROOM_LIST",{rooms:socket.rooms})
     }
 }
+
