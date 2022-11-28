cd9a43dfd5047dc431c79fe9183220f63380808b
Roman <roman.pohribniak@gmail.com>
2022-11-28 17:59:17 +0200

    add rooms

--- src/chat.geteway.ts
+++ src/chat.geteway.ts
@@ -5,22 +5,47 @@ import { Server, Socket } from 'socket.io';
 export class ChatGeteway implements OnGatewayConnection { 
     @WebSocketServer()
     server;
-
     async handleConnection(socket: Socket) {
-      console.log(socket.id)
+    }
+
+    @SubscribeMessage("CREATE_ROOM")
+    createRoom(socket: Socket,message:any){
+      this.enterRoom(socket,message);
+      this.getRooms(socket);
+    }
+    
+    @SubscribeMessage("GET_CONNECTED_SOCKETS")
+    getRoomSockets(socket: Socket,message:any){
+      const roomSockets = [];
+      this.server.sockets.adapter.rooms.get(message.roomId).forEach(socket => {
+        roomSockets.push(socket)
+      });
+      this.server.emit("ROOM_SOCKETS",{roomSockets})
+    }
+    
+    @SubscribeMessage("LEAVE_ROOM")
+    leaveRoom(socket: Socket,message:any){
+      socket.leave(message.id);
+      this.getRoomSockets(socket,message);
     }
 
     @SubscribeMessage("ENTER_ROOM")
     enterRoom(socket: Socket,message:any){
-      console.log(message.roomid);
-      socket.join(message.roomid);
+      socket.join(message.roomId);
       this.getRooms(socket);
     }
     
     @SubscribeMessage("GET_ROOMS")
     getRooms(socket: Socket): any{
-      console.log(socket.rooms)
-        this.server.emit("ROOM_LIST",{rooms:socket.rooms})
+        const rooms = [];
+        for (let entry of this.server.sockets.adapter.rooms.entries()){
+          if(entry[0].length != 20){
+            rooms.push(entry[0])
+          }
+        }
+        console.log(rooms)
+        
+        this.server.emit("ROOM_LIST",{rooms})
     }
 }
 
