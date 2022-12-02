ac517581b53e94944415027c07d0a582f9ca7dea
Roman <roman.pohribniak@gmail.com>
2022-12-02 23:16:29 +0200

    add chat

--- src/chat.geteway.ts
+++ src/chat.geteway.ts
@@ -1,6 +1,17 @@
 import { MessageBody,SubscribeMessage, WebSocketGateway,WebSocketServer, OnGatewayDisconnect} from "@nestjs/websockets";
 import { Server, Socket } from 'socket.io';
 
+const DB = {};
+
+class Chat {
+  participants:object;
+  messages:object;
+  constructor(participant){
+    this.participants = [participant];
+    this.messages = [];
+  }
+}
+
 @WebSocketGateway()
 export class ChatGeteway implements OnGatewayDisconnect { 
     @WebSocketServer()
@@ -9,42 +20,82 @@ export class ChatGeteway implements OnGatewayDisconnect {
     async handleDisconnect(socket: Socket){
     }
 
+    getSockets(roomId:string){
+      const roomSockets = [];
+      const {rooms} = this.server.sockets.adapter; 
+      if(rooms.get(roomId)){
+        rooms.get(roomId).forEach(socket => {
+          roomSockets.push(socket)
+        });
+      } 
+      return roomSockets;
+    }
+
+    createDBRoom(message:any){
+      if(!DB[message.roomId]){
+        DB[message.roomId] = new Chat(message.participant);
+        return true;
+      }
+      return false;
+    }
+
+    joinDBRoom(message:any){
+      if(!DB[message.roomId].participants.includes(message.participant)){
+        DB[message.roomId].participants.push(message.participant)
+      }
+    }
+
+    getRoomHistory({roomId}){
+      return DB[roomId].messages;
+    }
+
+    newDBMessage({roomId,sender,txt}){
+      DB[roomId].messages.push({sender,txt})
+    }
+
+    @SubscribeMessage("MESSAGE")
+    sendMessage(socket:Socket,message:any){
+      this.newDBMessage(message);
+      this.server.to(message.roomId).emit("NEW_MESSAGE",{txt:message.txt,sender:message.sender});
+    }
+
     @SubscribeMessage("CREATE_ROOM")
     createRoom(socket: Socket,message:any){
-      this.enterRoom(socket,message);
-      this.getRooms(socket);
+      if(this.createDBRoom(message)){
+        this.enterRoom(socket,message);
+        this.sendRooms();
+      }
     }
     
     @SubscribeMessage("GET_CONNECTED_SOCKETS")
-    getRoomSockets(socket: Socket,message:any){
-      const roomSockets = [];
-      const {rooms} = this.server.sockets.adapter; 
-      if(rooms.get(message.roomId)){
-        rooms.get(message.roomId).forEach(socket => {
-          roomSockets.push(socket)
-        });
-        this.server.to(message.roomId).emit("ROOM_SOCKETS",{roomSockets})
-      }
+    sendUsers(socket: Socket,message:any){
+        this.server.to(message.roomId).emit("ROOM_SOCKETS",{roomParticipants:DB[message.roomId].participants})
     }
     
     @SubscribeMessage("LEAVE_ROOM")
     leaveRoom(socket: Socket,message:any){
       socket.leave(message.roomId);
-      this.getRoomSockets(socket,message);
+      this.sendUsers(socket,message);
     }
 
     @SubscribeMessage("ENTER_ROOM")
     enterRoom(socket: Socket,message:any){
-      socket.join(message.roomId);
-      this.getRooms(socket);
+        this.createDBRoom(message)
+        this.joinDBRoom(message);
+        socket.join(message.roomId);
+        socket.to(socket.id).emit("MESSAGE_HISTORY",this.getRoomHistory(message))
+        this.sendRooms();
     }
     
     @SubscribeMessage("GET_ROOMS")
-    getRooms(socket: Socket): any{
+    sendRooms(): any{
         const rooms = [];
         for (let entry of this.server.sockets.adapter.rooms.entries()){
           if(entry[0].length != 20){
-            rooms.push(entry[0])
+            rooms.push({
+              id:entry[0],
+              sockets:this.getSockets(entry[0]),
+            })
           }
         }
         
