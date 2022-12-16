03675e10121235f25d5716483aad68e7974c9bd2
Roman <roman.pohribniak@gmail.com>
2022-12-16 21:04:45 +0200

    add route

--- dist/main.js
+++ dist/main.js
@@ -3,8 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function start() {
+    const PORT = process.env.PORT || 5000;
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(process.env.PORT);
+    await app.listen(PORT, () => {
+        console.log(`Server has been started on port:${PORT}`);
+    });
 }
 start();
 //# sourceMappingURL=main.js.map
