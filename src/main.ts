5f811fb15ebdeaef11ca981f4c147f9b3f88d6c2
Roman <roman.pohribniak@gmail.com>
2022-12-16 21:22:41 +0200

    add route

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 
 async function start() {
-  const PORT = process.env.PORT || 5000;
+  const PORT = `${process.env.PORT || 5000}`;
+  console.log(PORT);
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT,()=>{
     console.log(`Server has been started on port:${PORT}`);
